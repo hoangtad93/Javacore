0a65dda1bd5e21497fab3ef4dbc7ed5ebd790963
TaHoang <tahoang@TaHoangs-MacBook-Pro.local>
2023-01-30 00:17:33 +0900

    Testing_System

--- src/com/vti/fontend/demo4.java
+++ src/com/vti/fontend/demo4.java
@@ -2,6 +2,6 @@ package com.vti.fontend;
 
 public class demo4 {
 	public static void main(String[] args) {
-	System.out.println(" đây là bài test github");
+	System.out.println(" đây là bài test github bài này làm rất lâu mới sửa dk mệt!");
 }
 }
